2b4025ec815bfe6e81d25fbd2eb05b0ffa0b048c
BrandonMGH <BrandonMGH@Yahoo.com>
2020-03-29 14:09:53 -0700

    Client folder is now being served proerly to the express server

--- client/index.js
+++ client/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//** COMPONENTS **//
+import App from './src/app.js'
+
 const Index = () => {
  return (
     <div>
-        Index
+        <App />
     </div>
  )
   
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(express.static("client/assets"));
+app.use(express.json());
+app.use(express.static('dist'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './dist/index.html')); // relative path
+});
+
+app.listen(3000, () => {
+    console.log("Server is running")
+});
